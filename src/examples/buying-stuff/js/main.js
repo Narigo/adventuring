4f6e895b4f73a6b653400881b0bf51b8b16d1e29
Joern Bernhardt <jb@campudus.com>
2015-07-17 07:49:09 +0200

    Change scene on shop and table click, use money on pen
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/examples/buying-stuff/js/main.js
+++ src/examples/buying-stuff/js/main.js
@@ -6,6 +6,17 @@ import Item from '../../../main/js/Item'
 let adventure = new Adventure('My buying adventure');
 let inventory = new Inventory();
 let $inventory = document.getElementById('inventory');
+let itemInUse = null;
+
+adventure.on('change-scene', (data) => {
+  console.log(data);
+  if (data.oldScene) {
+    let $oldScene = document.getElementById(data.oldScene.background);
+    $oldScene.style.display = 'none';
+  }
+  let $newScene = document.getElementById(data.scene.background);
+  $newScene.style.display = 'block';
+});
 
 inventory.on('add', (item) => {
   let $item = document.createElement('div');
@@ -13,6 +24,25 @@ inventory.on('add', (item) => {
   item.$element.removeEventListener('click');
   $item.appendChild(item.$element);
   $inventory.appendChild($item);
+
+  item.$element.addEventListener('click', useItem(item));
+  document.addEventListener('click', removeUsing);
+
+  function useItem(item) {
+    return (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log('using something...');
+      itemInUse = item;
+    };
+  }
+
+  function removeUsing(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('not using anything');
+    itemInUse = null;
+  }
 });
 
 let outsideShop = new Scene('outside-shop');
@@ -31,6 +61,10 @@ let $pen = document.getElementById('pen');
 let pen = new Item('pen', $pen);
 $pen.addEventListener('click', () => {
   console.log('clicked on pen');
+  if (itemInUse === moneyItem) {
+    console.log('using money on pen');
+    moneyItem.use(pen);
+  }
 });
 
 moneyItem.on('use', {
@@ -47,3 +81,10 @@ let $door = document.getElementById('shop');
 $door.addEventListener('click', () => {
   adventure.setScene(insideShop);
 });
+
+let $doorOut = document.getElementById('table');
+$doorOut.addEventListener('click', () => {
+  adventure.setScene(outsideShop);
+});
+
+adventure.setScene(outsideShop);
