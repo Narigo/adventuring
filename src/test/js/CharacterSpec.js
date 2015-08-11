22ede17056e5c0504cfae828ebb3311cc50d0bc4
Joern Bernhardt <jb@campudus.com>
2015-08-11 23:42:02 +0200

    Refactor charName into name and use ES6 feature to collapse variable name in json
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/test/js/CharacterSpec.js
+++ src/test/js/CharacterSpec.js
@@ -3,22 +3,22 @@ import Character from '../../main/js/Character';
 import Dialog from '../../main/js/Dialog';
 
 describe('A character', () => {
-  let charName = 'Peter';
+  let name = 'Peter';
   let $element = document.createElement('div');
 
   it('should have a name', () => {
-    let peter = new Character('peter1', $element, {name : charName});
-    expect(peter.name).toEqual(charName);
+    let peter = new Character('peter1', $element, {name});
+    expect(peter.name).toEqual(name);
   });
 
   it('should be have a standard dialog color', () => {
-    let peter = new Character('peter1', $element, {name : charName});
+    let peter = new Character('peter1', $element, {name});
     expect(peter.color).toEqual('#ffffff');
   });
 
   it('should be possible to have a dialog color', () => {
     let color = '#00ff00';
-    let peter = new Character('peter1', $element, {name : charName, color});
+    let peter = new Character('peter1', $element, {name, color});
     expect(peter.color).toEqual(color);
   });
 
