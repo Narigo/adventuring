8f27734507ceeda4f4b33f7f1725d1f788c584be
Joern Bernhardt <jb@campudus.com>
2015-08-21 01:30:24 +0200

    Fix scripts task and use marked for html documentation
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var del = require('del');
 var es = require('event-stream');
-var ghPages = require('gulp-gh-pages');
 var fs = require('fs');
+var ghPages = require('gulp-gh-pages');
+var gutil = require('gulp-util');
+var markdown = require('gulp-markdown');
 var path = require('path');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
@@ -14,8 +16,9 @@ gulp.task('assets', copyAssets);
 gulp.task('sass', compileScss);
 gulp.task('scripts', compileScripts);
 gulp.task('build:core', ['assets', 'sass', 'scripts']);
-gulp.task('build:examples', ['build:core'], buildExampleList);
-gulp.task('build', ['build:core', 'build:examples']);
+gulp.task('build:examples', ['build:core'], buildIndexFile);
+gulp.task('build:docs', buildDocumentation);
+gulp.task('build', ['build:core', 'build:examples', 'build:docs']);
 gulp.task('clean', cleanOutDir);
 gulp.task('deploy', ['build:examples'], deployGhPages);
 gulp.task('default', ['build:examples']);
@@ -36,33 +39,30 @@ function compileScss() {
 function compileScripts(cb) {
   var scriptStreams = [];
   var basePath = path.resolve('src/examples');
-  console.log('starting gulp.src');
 
   gulp.src('src/examples/**/main.js')
-    .pipe(through.obj(function (file) {
+    .pipe(through.obj(function (file, enc, callback) {
       var relativePath = path.dirname(file.path).substring(basePath.length);
-      console.log('file=', file.path);
+
       var stream = browserify(file.path)
         .transform('babelify')
         .bundle()
-        .on('error', function (err) {
-          console.log('error occured:', err);
-          this.emit('end');
-        })
+        .on('error', gutil.log)
         .pipe(source('main.js'))
-        .pipe(gulp.dest(outDir + '/' + relativePath));
+        .pipe(gulp.dest(outDir + '/' + relativePath))
 
       scriptStreams.push(stream);
-      return stream;
+
+      callback();
     }))
+    .on('data', gutil.log)
     .on('end', function () {
-      console.log('stream ended');
       es.merge.apply(null, scriptStreams)
         .on('end', cb);
     });
 }
 
-function buildExampleList(cb) {
+function buildIndexFile(cb) {
   var examplesPath = 'src/examples';
   var subDirsHtml = fs.readdirSync(examplesPath).filter(function (file) {
     return fs.statSync(path.join(examplesPath, file)).isDirectory();
@@ -74,6 +74,12 @@ function buildExampleList(cb) {
 
   fs.writeFileSync('out/index.html', indexHtml);
   cb();
+}
+
+function buildDocumentation() {
+  return gulp.src('README.md')
+    .pipe(markdown())
+    .pipe(gulp.dest(outDir));
 }
 
 function deployGhPages() {
